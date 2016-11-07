8d052f42b6b721092453caab9173d6374d24de8a
Martin Jaime <martin.jaime-viveros@originate.com>
2016-11-07 09:40:43 -0800

    Handles incoming messages that need to be registered.

--- exocomMock.go
+++ exocomMock.go
@@ -10,8 +10,7 @@ import (
 
 type ExoCom struct {
 	ServerPort int
-	Services   map[string]websocket.Conn
-	done       chan bool
+	Services   map[string]*websocket.Conn
 }
 
 type Message struct {
@@ -31,10 +30,12 @@ type Service struct {
 }
 
 func New() *ExoCom {
-	http.Handle("/services", websocket.Handler(onConnected))
 	log.Println("EXOCOM: ExoCom initialized!")
-	doneChannel := make(chan bool)
-	return &ExoCom{0, nil, doneChannel}
+	return &ExoCom{0, make(map[string]*websocket.Conn)}
+}
+
+func (exocom *ExoCom) RegisterService(name string, ws *websocket.Conn) {
+	exocom.Services[name] = ws
 }
 
 func (exocom *ExoCom) Close() {
@@ -44,7 +45,19 @@ func (exocom *ExoCom) Close() {
 func (exocom *ExoCom) Listen(port int) {
 	log.Println("EXOCOM: Starting listener.")
 	exocom.ServerPort = port
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil /*websocket.Handler(onConnected)*/)
+	onMessage := func(ws *websocket.Conn) {
+		var incoming *Message
+		err := websocket.JSON.Receive(ws, &incoming)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if incoming.Name == "exocom.register-service" {
+			exocom.RegisterService(incoming.Sender, ws)
+		}
+	}
+
+	http.Handle("/services", websocket.Handler(onMessage))
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
