fc4810bc3a9f3b1c3baac003dd8d824f5acadeae
Martin Jaime <martin.jaime-viveros@originate.com>
2016-12-01 10:32:27 -0800

    WIP: A bit of refactoring to add basic features (#2)
    * Fixes listening to messages
    
    * Adds feature to close exocom

--- exocomMock.go
+++ exocomMock.go
@@ -2,36 +2,52 @@ package exocomMock
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"sync"
+	"time"
+
+	graceful "gopkg.in/tylerb/graceful.v1"
+
+	"github.com/fatih/color"
 
 	"golang.org/x/net/websocket"
 )
 
+var red = color.New(color.FgRed).SprintFunc()
+var cyan = color.New(color.FgHiCyan).SprintFunc()
+
 type ExoCom struct {
 	sync.Mutex
-	ServerPort       int
+	server           *graceful.Server
+	port             int
 	Services         map[string]*websocket.Conn
 	ReceivedMessages []Message
+	doneCh           chan bool
+	messageCh        chan Message
+	errCh            chan error
 }
 
 type Message struct {
-	Name         string      `json:"name"`
-	Sender       string      `json:"sender"`
-	Payload      interface{} `json:"payload"`
-	ResponseTo   string      `json:"responseTo"`
-	ID           string      `json:"id"`
-	Timestamp    int         `json:"timestamp"`
-	ResponseTime int         `json:"timestamp"`
+	Name         string      `json:"name,omitempty"`
+	Sender       string      `json:"sender,omitempty"`
+	Payload      interface{} `json:"payload,omitempty"`
+	ResponseTo   string      `json:"responseTo,omitempty"`
+	ID           string      `json:"id,omitempty"`
+	Timestamp    int         `json:"timestamp,omitempty"`
+	ResponseTime int         `json:"timestamp,omitempty"`
 }
 
 func New() *ExoCom {
 	log.Println("EXOCOM: ExoCom initialized!")
 	return &ExoCom{
-		ServerPort:       0,
+		port:             0,
 		Services:         make(map[string]*websocket.Conn),
 		ReceivedMessages: make([]Message, 0),
+		doneCh:           make(chan bool, 1),
+		messageCh:        make(chan Message),
+		errCh:            make(chan error),
 	}
 }
 
@@ -40,32 +56,72 @@ func (exocom *ExoCom) RegisterService(name string, ws *websocket.Conn) {
 }
 
 func (exocom *ExoCom) Close() {
+	exocom.doneCh <- true
+	exocom.server.Stop(100 * time.Millisecond)
+}
 
+func (exocom *ExoCom) listenToMessages(ws *websocket.Conn) {
+	go exocom.messageHandler(ws)
+	for {
+		select {
+		case <-exocom.doneCh:
+			exocom.doneCh <- true
+			return
+		case incoming := <-exocom.messageCh:
+			log.Printf(cyan("EXOCOM: MESSAGE RECEIVED in listenToMessages: %#v\n"), incoming)
+		}
+	}
+}
+
+func (exocom *ExoCom) messageHandler(batman *websocket.Conn) {
+	var incoming Message
+	for {
+		select {
+		case <-exocom.doneCh:
+			exocom.doneCh <- true
+			return
+		default:
+			err := websocket.JSON.Receive(batman, &incoming)
+			if err == io.EOF {
+				return
+			}
+			exocom.Lock()
+			exocom.saveMessage(incoming)
+			exocom.Unlock()
+			exocom.messageCh <- incoming
+		}
+	}
+}
+
+func (exocom *ExoCom) saveMessage(message Message) {
+	exocom.ReceivedMessages = append(exocom.ReceivedMessages, message)
 }
 
 func (exocom *ExoCom) Listen(port int) {
-	log.Println("EXOCOM: Starting listener.")
-	exocom.ServerPort = port
+	exocom.port = port
 
-	onMessage := func(ws *websocket.Conn) {
+	onConnection := func(ws *websocket.Conn) {
+		//ws.SetReadDeadline(time.Now().Add(1 * time.Second))
 		var incoming Message
-		err := websocket.JSON.Receive(ws, &incoming)
-		if err != nil {
-			log.Fatal(err)
+		if err := websocket.JSON.Receive(ws, &incoming); err != nil {
+			log.Fatal(red(err))
 		}
-		exocom.Lock()
 		if incoming.Name == "exocom.register-service" {
 			exocom.RegisterService(incoming.Sender, ws)
+			exocom.saveMessage(incoming)
+			exocom.listenToMessages(ws)
 		}
-		fmt.Printf("EXOCOM: ---- INCOMING MESSAGE ---- : %#v\n", incoming)
-		exocom.ReceivedMessages = append(exocom.ReceivedMessages, incoming)
-		exocom.Unlock()
 	}
 
-	http.Handle("/services", websocket.Handler(onMessage))
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
-	if err != nil {
-		log.Fatalln(err)
+	exocom.server = &graceful.Server{
+		Timeout: 1 * time.Second,
+		Server: &http.Server{
+			Addr:    fmt.Sprintf(":%d", port),
+			Handler: websocket.Handler(onConnection),
+		},
 	}
-	log.Println("EXOCOM: Listener is done")
+	err := exocom.server.ListenAndServe()
+	if err != nil {
+		log.Fatalln(red(err))
+	}
 }
